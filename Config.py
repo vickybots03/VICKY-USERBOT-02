5e9e7be72fe4509bd73f465693de8c9ac3b60353
vickybots03 <145100955+vickybots03@users.noreply.github.com>
2023-09-16 09:22:32 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itszshivam/OP-USERBOT2")
+REPO_URL = getenv("REPO_URL", "https://github.com/vickybots03/VICKY-USERBOT-02/tree/main")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")

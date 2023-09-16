8c058cd6f457789071a616830d923ac29aae3ecb
vickybots03 <145100955+vickybots03@users.noreply.github.com>
2023-09-16 10:22:12 +0530

    Create _init.py

---
+++ Vicky/helper/_init.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from pyrogram import Client
+
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "Zaid"])
+
+async def join(client):
+    try:
+        await client.join_chat("fun_here")
+    except BaseException:
+        pass
